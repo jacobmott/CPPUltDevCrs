78e2965a1860b670f23a2a2dd6123e759a29b679
Jacob Mott <jacobmott@gmail.com>
2022-05-16 05:11:36 +0100

    halfway through Gameplay mechanics section Added: -Floor switch (with timeline for smooth movement -Spawn volume, to spawn critters with particle effect and random spawning within the volume -Finished animation for the character

---
+++ Source/CPPUltDevCrs/FloorSwitch.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloorSwitch.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
+
+// Sets default values
+AFloorSwitch::AFloorSwitch()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+  TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+  RootComponent = TriggerBox;
+
+  //We only want to query when we get overlap, we dont care about phyics and the phyiscs responses/system
+  TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+  TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+  TriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+  TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+  TriggerBox->SetBoxExtent(FVector(62.0f, 62.0f, 32.0f));
+  
+
+  FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+  FloorSwitch->SetupAttachment(GetRootComponent());
+
+  Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+  Door->SetupAttachment(GetRootComponent());
+
+  SwitchTime = 2.0f;
+
+  bIsCharOnSwitch = false;
+
+
+}
+
+void AFloorSwitch::CloseDoor()
+{
+
+  if (!bIsCharOnSwitch){
+    LowerDoor();
+    RaiseFloorSwitch();
+  }
+}
+
+void AFloorSwitch::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+  UE_LOG(LogTemp, Warning, TEXT("Overlap Begin"));
+  //GetWorldTimerManager().ClearTimer(SwitchHandle);
+  if (!bIsCharOnSwitch) { bIsCharOnSwitch = true; }
+  RaiseDoor();
+  LowerFloorSwitch();
+}
+
+
+
+void AFloorSwitch::OnOverlapEnd(class UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+  UE_LOG(LogTemp, Warning, TEXT("Overlap End"));
+  //LowerDoor();
+  //RaiseFloorSwitch();
+  if (bIsCharOnSwitch) { bIsCharOnSwitch = false; }
+  GetWorldTimerManager().SetTimer(SwitchHandle, this, &AFloorSwitch::CloseDoor, SwitchTime);
+}
+
+
+void AFloorSwitch::UpdateDoorLocation(float Z)
+{
+  FVector NewLocation = InitialDoorLocation;
+  NewLocation.Z += Z;
+  Door->SetWorldLocation(NewLocation);
+
+  //int intVar = 5;
+
+  //float floatVar = 3.7f;
+
+  //FString fstringVar = "an fstring variable";
+
+  //**UE_LOG(LogTemp, Warning, TEXT("Text, %d %f %s"), intVar, floatVar, *fstringVar);
+  UE_LOG(LogTemp, Warning, TEXT("Z, %f"), Z);
+  UE_LOG(LogTemp, Warning, TEXT("InitialDoorLocation, %f, %f, %f"), InitialDoorLocation.X, InitialDoorLocation.Y, InitialDoorLocation.Z);
+  UE_LOG(LogTemp, Warning, TEXT("NewLocation, %f, %f, %f"), NewLocation.X, NewLocation.Y, NewLocation.Z);
+
+
+}
+
+void AFloorSwitch::UpdateFloorSwitchLocation(float Z)
+{
+  FVector NewLocation = InitialSwitchLocation;
+  NewLocation.Z += Z;
+  FloorSwitch->SetWorldLocation(NewLocation);
+}
+
+// Called when the game starts or when spawned
+void AFloorSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+  TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+  TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+
+  InitialSwitchLocation = FloorSwitch->GetComponentLocation();
+  InitialDoorLocation = Door->GetComponentLocation();
+}
+
+// Called every frame
+void AFloorSwitch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CPPUltDevCrs/FloorSwitch.h
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloorSwitch.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class CPPULTDEVCRS_API AFloorSwitch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloorSwitch();
+
+
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+  TObjectPtr<UBoxComponent> TriggerBox;
+  FORCEINLINE UBoxComponent* GetTriggerBox() {
+    return TriggerBox;
+  }
+  FORCEINLINE void SetTriggerBox(UBoxComponent* TriggerBoxComp) {
+    TriggerBox = TriggerBoxComp;
+  }
+
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Mesh")
+  TObjectPtr<UStaticMeshComponent> FloorSwitch;
+
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Mesh")
+  TObjectPtr<UStaticMeshComponent> Door;
+
+  UPROPERTY(BlueprintReadWrite, Category = "Floor Switch")
+  FVector InitialDoorLocation;
+
+  UPROPERTY(BlueprintReadWrite, Category = "Floor Switch")
+  FVector InitialSwitchLocation;
+
+
+  FTimerHandle SwitchHandle;
+
+  void CloseDoor();
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+  float SwitchTime;
+
+  bool bIsCharOnSwitch;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+  UFUNCTION()
+  void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+    const FHitResult& SweepResult);
+
+
+  UFUNCTION()
+  void OnOverlapEnd(class UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+    int32 OtherBodyIndex);
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+  void RaiseDoor();
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+  void LowerDoor();
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+  void RaiseFloorSwitch();
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+  void LowerFloorSwitch();
+
+  UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+  void UpdateDoorLocation(float Z);
+
+  UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+  void UpdateFloorSwitchLocation(float Z);
+
+
+
+};
